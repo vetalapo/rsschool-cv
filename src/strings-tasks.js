4d95fa65e30ead8a4b0f2dcd3e5834aea8b674c4
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-20 23:31:41 +0100

    feat: implement removeFirstOccurrences function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -154,8 +154,13 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const valueIndex = str.indexOf(value);
+  if (valueIndex < 0) {
+    return str;
+  }
+
+  return `${str.slice(0, valueIndex)}${str.slice(valueIndex + value.length)}`;
 }
 
 /**
