9884f7e6cef39d58602a37aac4bd87f6b57c0623
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-21 17:15:13 +0100

    feat: implement unbracketTag function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -487,8 +487,8 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, str.length - 1);
 }
 
 /**
