78bd0fddca5466d7d968f64971609f9696473986
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-21 21:49:39 +0100

    feat: implement getDistanceBetweenPoints function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -68,8 +68,11 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const a = x2 - x1;
+  const b = y2 - y1;
+
+  return Math.sqrt(a * a + b * b);
 }
 
 /**
