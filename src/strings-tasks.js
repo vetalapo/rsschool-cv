c25a7d9da437e7cc1cb71e82cb82c25daaede90f
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-21 16:45:17 +0100

    feat: add solution for reverseWords function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -415,8 +415,11 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  return str
+    .split(' ')
+    .map((w) => reverseString(w))
+    .join(' ');
 }
 
 /**
