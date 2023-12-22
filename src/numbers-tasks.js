28dae0b35a0dfbae8c4f2d30fb3febdc7c6de92e
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-22 23:36:31 +0100

    feat: implement getFibonacciNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -263,8 +263,12 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 1) {
+    return index;
+  }
+
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
