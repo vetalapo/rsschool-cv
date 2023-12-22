96c2ff3d41d622e76eca159079bde0c5f4bce9ef
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-23 00:17:43 +0100

    feat: ipmlement isPowerOfTwo function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -321,8 +321,8 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  return Number.isInteger(Math.log2(num));
 }
 
 /**
