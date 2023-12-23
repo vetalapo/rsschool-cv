63a7118866fe5c73f9fc0c3884cfeb9d23ef971d
Vitaly Dushkin <vetalapo@gmail.com>
2023-12-23 01:35:29 +0100

    feat: ipmlement getCountOfOddNumbers function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -610,8 +610,8 @@ function getRandomInteger(min, max) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -627,8 +627,8 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  return Math.round(Math.abs(number) / 2);
 }
 
 module.exports = {
