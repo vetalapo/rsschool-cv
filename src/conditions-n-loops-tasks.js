5a1ee5105fad0c8c9fa301885730f234bfab1c64
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-22 00:07:49 +0100

    feat: implement convertToRomanNumerals function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -115,8 +115,26 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let tensResult = '';
+  for (let i = 0; i < Math.trunc(num / 10); i += 1) {
+    tensResult += 'X';
+  }
+
+  const digitsMap = {
+    0: '',
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+  };
+
+  return `${tensResult}${digitsMap[num % 10]}`;
 }
 
 /**
