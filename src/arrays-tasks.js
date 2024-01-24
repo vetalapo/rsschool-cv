b14ebf0a9a680a6ef18f04168cc54feebf6a22ea
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-24 03:27:16 +0100

    feat: implement createNDimensionalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -265,8 +265,14 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n === 1) {
+    return new Array(size).fill(0);
+  }
+
+  return Array.from({ length: size }).map(() =>
+    createNDimensionalArray(n - 1, size)
+  );
 }
 
 /**
