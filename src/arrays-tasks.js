2e6f3b493d284bc259cd701845116ace9ce46179
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-24 18:42:58 +0100

    feat: implement getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -432,8 +432,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((value, index) => (value % 2 === 0 ? undefined : index))
+    .filter((x) => x !== undefined);
 }
 
 /**
