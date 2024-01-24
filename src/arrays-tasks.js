771ef19b1493ad0ac91550b2f5a0d708203b00a0
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-24 03:57:36 +0100

    feat: implement createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -336,8 +336,10 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return [...Array(Math.ceil(arr.length / chunkSize))].map(() =>
+    arr.splice(0, chunkSize)
+  );
 }
 
 /**
