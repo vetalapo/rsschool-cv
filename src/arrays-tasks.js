2e61386868bd2e21e6847984a65638d7c0be7ec2
Vitaly Dushkin <vetalapo@gmail.com>
2024-01-24 04:11:22 +0100

    feat: implement getElementByIndices

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -370,8 +370,12 @@ function generateOdds(len) {
  *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
  *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
  */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  if (indices.length === 1) {
+    return arr[indices[0]];
+  }
+
+  return getElementByIndices(arr[indices[0]], indices.slice(1));
 }
 
 /**
