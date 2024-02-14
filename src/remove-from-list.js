de32cf88579bca07cc06129b0d7fb9e41cb3b20d
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-14 21:57:37 +0100

    feat: implement Remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,7 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
-
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
@@ -22,9 +18,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+  
+  let lCopy = l;
+
+  while(l.next != null) {
+    if (l.next.value === k) {
+      l.next = l.next.next;
+      continue;
+    }
+
+    l = l.next;
+  }
+
+  return lCopy;
 }
 
 module.exports = {
