e8bf7d7b232e7546774fa7c0c434ec05e92e0298
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-14 20:26:57 +0100

    feat: implemnt Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +11,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue extends ListNode {
+  constructor() {
+    super(null);
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this;
+  }
+
+  enqueue(value) {
+    if (value === null) {
+      return;
+    }
+
+    const addValue = (node, value) => {
+      if (node.value === null) {
+        node.value = value;
+      } else {
+        if (node.next === null) {
+          node.next = new ListNode(value);
+        } else {
+          addValue(node.next, value);
+        }
+      }
+    }
+
+    addValue(this, value);
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.value === null) {
+      return;
+    }
+
+    const valueCopy = this.value;
+    const nextCopy = this.next;
+
+    this.value = nextCopy.value;
+    this.next = nextCopy.next;
+
+    return valueCopy;
   }
 }
 
