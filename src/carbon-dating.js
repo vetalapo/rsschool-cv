0f7242639539a3fbea59657f563eb0fdb927e34b
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-15 01:00:46 +0100

    feat: implemnt carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
@@ -17,9 +15,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+
+  if (!activity || activity <= 0) {
+    return false;
+  }
+
+  const halfTime = Math.LN2 / HALF_LIFE_PERIOD;
+  const result = Math.ceil(Math.log(MODERN_ACTIVITY / activity) / halfTime);
+
+  return result > 0 ? result : false;
 }
 
 module.exports = {
