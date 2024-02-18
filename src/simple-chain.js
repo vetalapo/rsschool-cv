0e6397e0a8012a4f7a33d67e17f79c922073717e
Vitaly Dushkin <vetalapo@gmail.com>
2024-02-18 01:13:32 +0100

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,29 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement chainMaker object according to task description
  * 
  */
 const chainMaker = {
+  values: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.values.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.values.push(`( ${String(value) || ''} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!Number.isInteger(position) || position < 1 || position > this.values.length) {
+      this.values = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    this.values.splice(position - 1, 1);
+
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.values.reverse();
+
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const chainCopy = this.values;
+    this.values = [];
+
+    return chainCopy.join("~~");
   }
 };
 
