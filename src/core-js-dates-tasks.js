7e2aa8536f54c0919da5ed2ab3bfb7111a537eba
Vitaly Dushkin <vetalapo@gmail.com>
2024-03-20 23:33:04 +0100

    feat: implement formatDate function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -170,8 +170,10 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const currLocaleDate = new Date(date);
+
+  return currLocaleDate.toLocaleString('en-US', { timeZone: 'UTC' });
 }
 
 /**
