a0d6d58f48ad0f80450d6ffa9e90fe7ffa83171a
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-04 01:15:45 +0200

    feat: implement queuPromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -140,8 +140,18 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  const enqueue = (index = 0, accumulator = '') => {
+    if (index === promises.length) {
+      return accumulator;
+    }
+
+    return promises[index].then((value) => {
+      return enqueue(index + 1, `${accumulator}${value}`);
+    });
+  };
+
+  return enqueue();
 }
 
 module.exports = {
