a2d1ee5553238d50f0104f6f7d890b3d72c5f63f
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-22 04:06:32 +0200

    feat: partialUsingArguments implementation

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -200,8 +200,8 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...args1, ...args2);
 }
 
 /**
