1b98d528cdacf87c1f3451a58a9ac62fa9c0dc70
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-22 04:12:52 +0200

    feat: getIdGeneratorFunction implementation

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -221,8 +221,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currId = startFrom;
+
+  return () => {
+    currId += 1;
+    return currId - 1;
+  };
 }
 
 module.exports = {
