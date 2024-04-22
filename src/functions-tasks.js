0ff0e0e4ebea8a0bc5b7fb52919dfb8cd144013f
Vitaly Dushkin <vetalapo@gmail.com>
2024-04-22 03:14:53 +0200

    feat: memoize implementation

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -106,8 +106,19 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  return (...args) => {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const currRes = func.apply(this, args);
+    cache[args] = currRes;
+
+    return currRes;
+  };
 }
 
 /**
