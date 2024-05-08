417ee7505d880927ac8c3d29117cf220b315d0e1
Vitaly Dushkin <vetalapo@gmail.com>
2024-05-08 18:18:16 +0200

    feat: typescript + vue initial setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    defineProps<{
+        msg: string
+    }>()
+</script>
+
+<template>
+    <div class="greetings">
+        <h1 class="green">{{ msg }}</h1>
+        <h3>The project has successfully been created</h3>
+    </div>
+</template>
